89a0f8b9ef255b33a00d3b6ada53a02b707471c7
Bohdan <bohdan.ko@starkey.co.il>
2022-06-21 15:24:18 +0300

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <VideoStream
+        video-id="5d5bc37ffcf54c9b82e996823bffbb81"
+        class="video"
+        autoplay muted loop
+    />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VideoStream from '@/VideoStream.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VideoStream
+  }
+});
+</script>
+
+<style>
+.video {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/VideoStream.vue
@@ -0,0 +1,114 @@
+<template>
+  <video ref="streamEl"/>
+</template>
+
+<script>
+import Hls from 'hls.js'
+
+const PROPS_TO_SYNC = [
+  'autoplay',
+  'controls',
+  'currentTime',
+  'muted',
+  'loop',
+  'volume',
+  'preload',
+]
+
+export default {
+  name: 'VideoStream',
+  props: {
+    /**
+     * The video id for the video you’ve uploaded to Cloudflare Stream should be included here.
+     */
+    videoId: {
+      type: String,
+      default: null
+    },
+    /**
+     * Tells the browser to immediately start downloading the video and play it as soon as it can. Note that mobile browsers generally do not support this attribute, the user must tap the screen to begin video playback. Please consider mobile users or users with Internet usage limits as some users don’t have unlimited Internet access before using this attribute.
+     *
+     * To disable video autoplay, the autoplay attribute needs to be removed altogether as this attribute. Setting autoplay="false" will not work; the video will autoplay if the attribute is there in the <stream> tag.
+     *
+     * In addition, some browsers now prevent videos with audio from playing automatically. You may add the mute attribute to allow your videos to autoplay. For more information, [go here](https://webkit.org/blog/6784/new-video-policies-for-ios/).
+     *
+     * @default false
+     */
+    autoplay: {type: Boolean, default: false},
+    /**
+     * Shows the default video controls such as buttons for play/pause, volume controls. You may choose to build buttons and controls that work with the player.
+     *
+     * @default false
+     */
+    controls: {type: Boolean, default: false},
+    /**
+     * Returns the current playback time in seconds. Setting this value seeks the video to a new time.
+     *
+     * @default 0
+     */
+    currentTime: {type: Number, default: 0},
+    /**
+     * A Boolean attribute which indicates the default setting of the audio contained in the video. If set, the audio will be initially silenced.
+     *
+     * @default false
+     */
+    muted: {type: Boolean, default: false},
+    /**
+     * A Boolean attribute; if included the player will automatically seek back to the start upon reaching the end of the video.
+     *
+     * @default false
+     */
+    loop: {type: Boolean, default: false},
+    /**
+     * This enumerated attribute is intended to provide a hint to the browser about what the author thinks will lead to the best user experience. You may choose to include this attribute as a boolean attribute without a value, or you may specify the value preload="auto" to preload the beginning of the video. Not including the attribute or using preload="metadata" will just load the metadata needed to start video playback when requested.
+     *
+     * The <video> element does not force the browser to follow the value of this attribute; it is a mere hint. Even though the preload="none" option is a valid HTML5 attribute, Stream player will always load some metadata to initialize the player. The amount of data loaded in this case is negligable.
+     */
+    preload: {
+      type: [String, Boolean],
+      default: 'auto',
+      validator(value) {
+        return ['auto', 'metadata', 'none'].includes(value) || (typeof value === 'boolean' && !!value)
+      }
+    },
+    /**
+     * Sets volume from 0.0 (silent) to 1.0 (maximum value)
+     *
+     * @default 1
+     */
+    volume: {type: Number, default: 1},
+  },
+  data() {
+    return {
+      hls: null
+    }
+  },
+  mounted() {
+    this.initPlayer()
+  },
+  methods: {
+    updateProp(key, value) {
+      if (!this.$refs.streamEl) return
+
+      this.$refs.streamEl[key] = value
+    },
+    initPlayer() {
+      if (!this.$refs.streamEl) return
+
+      PROPS_TO_SYNC.forEach(prop => {
+        this.updateProp(prop, this[prop])
+      })
+
+      this.hls = new Hls()
+      this.hls.loadSource(`https://videodelivery.net/${this.videoId}/manifest/video.m3u8`)
+      this.hls.attachMedia(this.$refs.streamEl)
+    }
+  }
+}
+</script>
+
+<style>
+.vjs-error-display .vjs-modal-dialog-content {
+  display: none
+}
+</style>
